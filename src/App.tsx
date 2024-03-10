83faca8e991b0bcb2c0a163bb564258ba89fa18f
Walid Helaoui <he.walid@gmail.com>
2024-03-10 21:07:35 +0100

    refactor: refactor style of app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import './App.css'
+import Header from './components/ui/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>test page</h1>
-      <div className="card">
-        <Button variant="outline" onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <Header />
+      <div className="contianer">
+        <h1>test page</h1>
+        <div className="card">
+          <Button
+            variant="outline"
+            onClick={() => setCount(count => count + 1)}
+          >
+            count is {count}
+          </Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
     </>
   )
